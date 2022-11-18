652d70183d5250052852c3419878093d5e290095
PESolut <jahaadpetty@pursuit.org>
2022-11-18 12:06:07 -0500

    searched video results logged within recommendedVideos.js component

--- src/Components/RecommendedVideos.js
+++ src/Components/RecommendedVideos.js
@@ -3,11 +3,13 @@ import { useState, useEffect } from 'react';
 import VideoCard from './VideoCard'
 import './RecommendedVideos.css'
 
-const RecommendedVideos = ({ searchInput, setSearchInput }) => {
+const RecommendedVideos = ({ searchInput, setSearchInput, searchedVideos }) => {
+    // console.log(searchInput)
     // useEffect(() => {
     //     fetchData()
     //         .then((data) => console.log(data))
     // },[])
+    console.log('Searched Videos State in RV comp: ',searchedVideos)
 
     return (
         <div className='recommendedVideos'>
--- src/Fetch.js
+++ src/Fetch.js
@@ -8,6 +8,6 @@ export function fetchData(searchBox) {
   const searchEndpoint = "?part=snippet&maxResults=10" 
     return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchBox}&maxResults=10${key}`)
                 .then((res) => res.json())
-                .then((res) => {console.log(res)})
-                .catch((err) => {console.log(err)});
+                // .then((res) => {console.log(res)})
+                // .catch((err) => {console.log(err)});
 }
