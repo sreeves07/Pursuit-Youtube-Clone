84b2d4f4e8dbd3c3617f5fdb639c3e6b401fa275
PESolut <jahaadpetty@pursuit.org>
2022-11-18 11:03:19 -0500

    Search changes

---
+++ Copy_App.js
@@ -0,0 +1,134 @@
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import Header from './Components/Header';
+import About from './Landing Pages/About';
+import Home from './Landing Pages/Home';
+import VideoCard from './Components/VideoCard';
+import Sidebar from './Components/Sidebar'
+import RecommendedVideos from './Components/RecommendedVideos';
+import './App.css';
+import { fetchData } from './Fetch';
+
+function App() {
+  const [searchBox, setSearchBox] = useState("")
+  const [searched, setSearched] = useState(false)
+  const [searchedVideos, setSearchedVideos] = useState([])
+
+  const handleUserSearch = (e) => {
+    setSearched(true)
+    e.preventDefault()
+    setSearchBox(e.target.value)
+    console.log('search submission: ',{searchBox})
+  }
+
+  const handleUserTextChange = (e) => {
+    e.preventDefault()
+    setSearchBox(e.target.value)
+    console.log('search change: ',{searchBox})
+  }
+
+    useEffect(() => {
+        if(searched) {
+          setSearched(false)
+            fetchData(searchBox)
+                .then((res) => {
+                    setSearchInput(res)
+                    setSearchedVideos(res)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    })
+  
+
+  // console.log(fetchData())
+  const [searchInput, setSearchInput] = useState('')
+
+  return (
+    <div className="App">
+      <Router>
+      <Header setSearchInput={setSearchInput} searchBox={searchBox} handleUserSearch={handleUserSearch} handleUserTextChange={handleUserTextChange}/>
+      <div className="app__page">
+        <Sidebar />
+        <Routes>
+        <Route path="/" element={<RecommendedVideos/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/videos/:id" element={<VideoCard searchInput={searchInput}/>} />
+        <Route path="/videos" element={<RecommendedVideos />} />
+        </Routes>
+      </div>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import Header from './Components/Header';
+import About from './Landing Pages/About';
+import Home from './Landing Pages/Home';
+import VideoCard from './Components/VideoCard';
+import Sidebar from './Components/Sidebar'
+import RecommendedVideos from './Components/RecommendedVideos';
+import './App.css';
+import { fetchData } from './Fetch';
+
+function App() {
+  const [searchBox, setSearchBox] = useState("")
+  const [searched, setSearched] = useState(false)
+  const [searchedVideos, setSearchedVideos] = useState([])
+
+  const handleUserSearch = (e) => {
+    setSearched(true)
+    e.preventDefault()
+    setSearchBox(e.target.value)
+    console.log('search submission: ',{searchBox})
+  }
+
+  const handleUserTextChange = (e) => {
+    e.preventDefault()
+    setSearchBox(e.target.value)
+    console.log('search change: ',{searchBox})
+  }
+
+    useEffect(() => {
+        if(searched) {
+          setSearched(false)
+            fetchData(searchBox)
+                .then((res) => {
+                    setSearchInput(res)
+                    setSearchedVideos(res)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    })
+  
+
+  // console.log(fetchData())
+  const [searchInput, setSearchInput] = useState('')
+
+  return (
+    <div className="App">
+      <Router>
+      <Header setSearchInput={setSearchInput} searchBox={searchBox} handleUserSearch={handleUserSearch} handleUserTextChange={handleUserTextChange}/>
+      <div className="app__page">
+        <Sidebar />
+        <Routes>
+        <Route path="/" element={<RecommendedVideos/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/videos/:id" element={<VideoCard searchInput={searchInput}/>} />
+        <Route path="/videos" element={<RecommendedVideos />} />
+        </Routes>
+      </div>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ Copy_Header.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+import './Header.css'
+
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
+
+import { fetchData } from '../Fetch';
+
+
+
+const Header = ({ setSearchInput, searchBox, handleUserSearch, handleUserTextChange }) => {
+    
+    return (
+        <div className="header">
+
+            <div className="header__left">
+                <MenuIcon />
+                <img 
+                className="header__logo"
+                src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg' 
+                alt='logo' 
+                />
+            </div>
+
+                <form className="header__input"
+                    
+                    onSubmit={handleUserSearch}>
+                    <input
+                        value={searchBox} 
+                        onChange={(e) => handleUserTextChange(e)}
+                        type="text" 
+                        placeholder="Search" />
+                    <button type="submit">
+                        <SearchIcon className='header__inputButton'/>
+                    </button>
+                </form>
+            
+
+                
+
+            <div className='header__icons'>
+                <VideoCallIcon className='header_icon'/>
+                <AppsIcon className='header_icon'/>
+                <NotificationIcon className='header_icon'/>
+                <Avatar
+                className="profile__photo" src='https://www.flaticon.com/free-icon/profile_3135715'
+                alt="profile icon"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
