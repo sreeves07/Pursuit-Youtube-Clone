5527a5a4efd4fe6c7f5af27030be5f554d227a37
PESolut <jahaadpetty@pursuit.org>
2022-11-18 13:03:43 -0500

    Example data rendered on RV comp

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -3,10 +3,17 @@ import Avatar from "@material-ui/core/Avatar"
 
 
 
-const VideoCard = ({ }) => {
+const VideoCard = ({ video }) => {
+
+    console.log(video)
     return ( 
-        <>
-        </>
+        <article className="video">
+            {/* <h1>test</h1> */}
+      <h3>
+        {video.snippet.title}
+      </h3>
+      <p className="description">{video.snippet.description}</p>
+    </article>
     )
 }
 
