b87711ee217df42f80d171cd80f01459c3766a6b
sreeves07 <shamarreeves@pursuit.org>
2022-11-28 12:54:53 -0500

    fetch

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,7 +1,6 @@
 //Fetch Youtube Data using Api Key
 
 export function fetchData(searchBox) {
-  // const key = `&key=${process.env.REACT_APP_YOUTUBE_KEY}`
     return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchBox}&maxResults=15&key=${process.env.REACT_APP_YOUTUBE_KEY}`)
                 .then((res) => res.json())
 }
