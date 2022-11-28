914d4713a70dcc203cc1357bad69630359617e7e
sreeves07 <shamarreeves@pursuit.org>
2022-11-28 12:47:21 -0500

    updating netlify fetch

--- src/Fetch.js
+++ src/Fetch.js
@@ -2,6 +2,6 @@
 
 export function fetchData(searchBox) {
   const key = `&key=${process.env.REACT_APP_YOUTUBE_KEY}`
-    return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchBox}&maxResults=15${key}`)
+    return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchBox}&maxResults=15&key=${process.env.REACT_APP_YOUTUBE_KEY}`)
                 .then((res) => res.json())
 }
